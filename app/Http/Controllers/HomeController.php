11b115c0f2b66512c2ffae66c9a93de135290b78
superirale <superirale@gmail.com>
2017-04-21 22:23:25 +0100

    latest chnanges

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Order;
+
+use App\Models\UserAddress;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class HomeController extends Controller
 {
     /**
