fef46a6fac18d3bcb34fc47e3334324ae2a1091c
superirale <superirale@gmail.com>
2017-04-27 16:45:52 +0100

    latest changes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         '/cart/add',
-        '/admin/update_status/{id}',
+        '/admin/update_status/*',
         '/addresses/*/update'
     ];
 }
