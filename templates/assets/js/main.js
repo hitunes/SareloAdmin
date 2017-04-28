0e16399e0adc3bee3b301c820c961279683964da
Samson Iyanda <samsoniyanda@outlook.com>
2017-04-28 18:16:55 +0100

    added a little css and edit js

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -554,7 +554,9 @@ const app = {
   radioChooser: function(para){
     $(document).find("input[type='radio']").on('change', function(){
       $(document).find("." + para).removeClass("" + para);
+      $(document).find(".fa-check-circle-o").hide();
        $(this).parent().addClass(""+ para);
+       $(this).parent().find(".fa-check-circle-o").show();
     });
   },
   buttonChooser: function(){
@@ -815,5 +817,77 @@ const app = {
               initRightMenu();   
           }
       });    
+  },
+  copyLinkCtrl: function(){
+    document.getElementById("copyButton").addEventListener("click", function() {
+    copyToClipboardMsg(document.getElementById("copyTarget"), "copyButton");
+    });
+
+    function copyToClipboardMsg(elem, msgElem) {
+        var succeed = copyToClipboard(elem);
+        var msg;
+        if (!succeed) {
+            msg = "Press Ctrl+c to copy."
+        } else {
+            msg = "Copied."
+        }
+        if (typeof msgElem === "string") {
+            msgElem = document.getElementById(msgElem);
+        }
+        msgElem.textContent = msg;
+        setTimeout(function() {
+            msgElem.textContent = "copy link";
+        }, 2000);
+    }
+
+    function copyToClipboard(elem) {
+          // create hidden text element, if it doesn't already exist
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+        
+        // copy the selection
+        var succeed;
+        try {
+              succeed = document.execCommand("copy");
+        } catch(e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+        
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+        return succeed;
+    }
   }
 }
