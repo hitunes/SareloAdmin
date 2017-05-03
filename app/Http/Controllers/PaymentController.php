1d7b8c7a5049b9d15fcfc8484fe7405762c3e9a7
superirale <superirale@gmail.com>
2017-05-03 12:17:32 +0100

    latest changes

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -16,6 +16,7 @@ use App\Mail\OrderInvoice;
 
 use \Cart;
 
+use Auth;
 
 class PaymentController extends Controller
 {
@@ -53,7 +54,7 @@ class PaymentController extends Controller
         Cart::destroy();
 
         //mail user
-        Mail::to($request->user())->send(new OrderInvoice($order));
+        Mail::to(Auth::user())->send(new OrderInvoice($order));
 
         return view('payment.complete', compact('order'));
     }
